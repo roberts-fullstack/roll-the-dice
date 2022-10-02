f4696b006195e575b45371c80bf9a41e90f1c2f9
Robert S <112721454+roberts-fullstack@users.noreply.github.com>
2022-10-02 19:36:25 -0400

    updated rigged-dice.js to use array

--- rigged-dice.js
+++ rigged-dice.js
@@ -1,13 +1,19 @@
+
 const prompt = require('prompt-sync')({sigint: true});
 
-let riggedNumber = Number(prompt('Please choose a rigged number between 1 and 6: >> '));
+let riggedNumber = Number(prompt("Choose one die side/number, to boost: "));
+
+const dieArray = [1,2,3,4,5,6];
+dieArray.push (riggedNumber); 
+
+let rollPosition = (Math.ceil(Math.random() * 7))
+let roll = dieArray[rollPosition];
 
 
-let roll = (Math.ceil(Math.random() * 7))
+if (roll === riggedNumber){
+  console.log("Your rigged number of " + riggedNumber + " has been rolled.");
+}
 
-
-if(roll === 7){
-  console.log("Your rigged number of" + riggedNumber + "has been rolled.");
-}else{
+else {
   console.log(roll);
 }
